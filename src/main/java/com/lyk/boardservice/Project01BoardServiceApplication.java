378c850dac73770c6fdb5003cd61e7bcef56a69d
hoochh <dbsrudwin@gmail.com>
2023-03-24 15:43:13 +0900

    #9 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기
    들어간 초기 기술:
    
    * Spring Web
    * Lombok
    * Actuator
    * DevTools

---
+++ src/main/java/com/lyk/boardservice/Project01BoardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lyk.boardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project01BoardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project01BoardServiceApplication.class, args);
+	}
+
+}
